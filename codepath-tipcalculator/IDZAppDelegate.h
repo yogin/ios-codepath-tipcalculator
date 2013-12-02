7239196aba91d3c9551e3c97bd610000ef1a143f
Anthony Powles <github@idreamz.net>
2013-12-02 15:45:04 -0800

    Initial Commit

---
+++ codepath-tipcalculator/IDZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IDZAppDelegate.h
+//  codepath-tipcalculator
+//
+//  Created by Anthony Powles on 12/2/13.
+//  Copyright (c) 2013 Anthony Powles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ codepath-tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  codepath-tipcalculator
+//
+//  Created by Anthony Powles on 12/2/13.
+//  Copyright (c) 2013 Anthony Powles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IDZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IDZAppDelegate class]));
+    }
+}

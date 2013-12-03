2a4e8a3ca11ca4e168e6858178e8aed605f45999
Anthony Powles <github@idreamz.net>
2013-12-02 16:41:49 -0800

    updated tip view to calculate tip and update various amounts

--- codepath-tipcalculator/IDZTipViewController.m
+++ codepath-tipcalculator/IDZTipViewController.m
@@ -16,6 +16,7 @@
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 
 - (IBAction)onTap:(id)sender;
+- (void)updateValues;
 
 @end
 
@@ -25,7 +26,6 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
         self.title = @"Tip Calculator";
     }
     return self;
@@ -34,17 +34,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [self updateValues];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)onTap:(id)sender {
+- (IBAction)onTap:(id)sender
+{
     [self.view endEditing:YES];
+    [self updateValues];
+}
+
+- (void)updateValues
+{
+    float billAmount = [self.billTextField.text floatValue];
+
+    NSArray *tipValues = @[@(0.1), @(0.15), @(0.2)];
+    float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
+    
+    float totalAmount = billAmount + tipAmount;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
 }
 
 @end
